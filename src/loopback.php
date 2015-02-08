1ae677e3b365cf2d280750b8d3b32d496b8ef0c4
Billy Kerns <bkerns77@gmail.com>
2015-02-08 12:54:46 -0600

    fixed empty request error

--- src/loopback.php
+++ src/loopback.php
@@ -6,51 +6,68 @@ echo '<!DOCTYPE html>
 <title>loopback</title>
 </head>
 <body>';
-//$getTester = 0;
+$getTester = 0;
 $postTester = 0;
-if($_GET){
-	$holder = array("Type" => "GET");
-	$parameters = array();
-	foreach($_GET as $key=>$value){
-		if($value == '0'){
-			$value = '0';
-		}
-		elseif(empty($value) == true){
-			$value = "undefined";
-		}
-		$parameters[$key] = $value;
-	}
-	$holder["parameters"] = $parameters;
-	$jsonHolder = json_encode($holder);
-	echo $jsonHolder;
-	$postTester++;
-	$getTester++;
-}
-elseif($_POST){
-	$holder = array("Type" => "POST");
-	$parameters = array();
-	foreach($_POST as $key=>$value){
-		if($value == '0'){
-			$value = '0';
-		}
-		elseif(empty($value) == true){
-			$value = "undefined";
-		}
-		$parameters[$key] = $value;
-	}
-	$holder["parameters"] = $parameters;
-	$jsonHolder = json_encode($holder);
-	echo $jsonHolder;
-	$postTester++;
-}
-if(empty($_GET) && $postTester < 1){
-	$holder = array(
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+	if(empty($_GET)){
+		$holder = array(
 		"Type" => "GET",
 		"parameters" => "null"
 	);
 	echo json_encode($holder);
+	}
+	else{
+		$holder = array("Type" => "GET");
+		$parameters = array();
+		foreach($_GET as $key=>$value){
+			if($value == '0'){
+				$value = '0';
+			}
+			elseif(empty($value) == true){
+				$value = "undefined";
+			}
+			$parameters[$key] = $value;
+		}
+		$holder["parameters"] = $parameters;
+		$jsonHolder = json_encode($holder);
+		echo $jsonHolder;
+		$getTester++;
+	}
 }
-//if(empty($_POST) && $getTester < 1){
+elseif($_SERVER['REQUEST_METHOD'] === 'POST'){
+	if(empty($_POST)){
+		$holder = array(
+		"Type" => "POST",
+		"parameters" => "null"
+	);
+	echo json_encode($holder);
+	}
+	else{
+		$holder = array("Type" => "POST");
+		$parameters = array();
+		foreach($_POST as $key=>$value){
+			if($value == '0'){
+				$value = '0';
+			}
+			elseif(empty($value) == true){
+				$value = "undefined";
+			}
+			$parameters[$key] = $value;
+		}
+		$holder["parameters"] = $parameters;
+		$jsonHolder = json_encode($holder);
+		echo $jsonHolder;
+		$postTester++;
+	}
+}
+//if($_SERVER['REQUEST_METHOD'] === 'GET') && empty($_GET) && $postTester < 1){
+//	$holder = array(
+//		"Type" => "GET",
+//		"parameters" => "null"
+//	);
+//	echo json_encode($holder);
+//}
+//if($_SERVER['REQUEST_METHOD' === 'POST']) && empty($_POST) && $getTester < 1){
 //	$holder = array(
 //		"Type" => "POST",
 //		"parameters" => "null"
